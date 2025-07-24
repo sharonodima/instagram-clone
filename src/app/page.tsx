6a80b0b82f959b564dcc01919b3ec0cbdb4eaae2
Sharon Odima <odimasharon@gmail.com>
2025-07-24 17:57:53 -0400

    Implement login form with JWT authentication and context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import React, { JSX } from "react";
-import NavBar from "@/components/NavBar";
 import Feed from "@/components/Feed";
 
 export default function Home(): JSX.Element {
   return (
     <div className="max-w-screen-md mx-auto px-4">
-      <NavBar />
       <Feed />
     </div>
   );
