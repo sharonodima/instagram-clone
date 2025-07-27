e442d621ef0879fd3b34a04c7bf4f6f651e12c5c
Sharon Odima <odimasharon@gmail.com>
2025-07-27 19:40:05 -0400

    Removed backend to separate repo

--- backend/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-const router = Router();
-
-router.get('/', (_req: Request, res: Response) => {
-  res.send('Auth route working!');
-});
-
-export default router;
