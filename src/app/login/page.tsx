f26e71e6f544055c80ac20b75ff6c048159fe3f1
Sharon Odima <odimasharon@gmail.com>
2026-09-19 16:27:30 -0400

    Update frontend API URL

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await fetch("https://instagram-clone-backend-lrh6.onrender.com", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -15,7 +15,7 @@ export default function RegisterPage() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
